ae7df9290693b863abffbcd050143333d94dbc63
Jaroslaw Pieprzak <jarekpiep@gmail.com>
2017-12-13 16:29:30 +0100

    create index and working wersion of wykresy

--- src/index.js
+++ src/index.js
@@ -1,8 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from 'react-router-dom';
+
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap-theme.css';
+
 import './index.css';
+
 import App from './App';
+import Wykresy from './Wykresy'
+
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+ReactDOM.render(
+  <Router>
+    <div>
+
+
+      <switch>
+        <Route exact path="/" component={App}/>
+        <Route path="/Wykresy" component={Wykresy}/>
+      </switch>
+
+      <p>&copy; 2017 BrodaczeGroup</p>
+    </div>
+  </Router>,
+  document.getElementById('root')
+);
 registerServiceWorker();
