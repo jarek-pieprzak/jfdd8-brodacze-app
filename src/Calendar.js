7feb347f952a42bd591469e79edec91d1f11cd49
Tomek Buszman <tomek.buszman0309@gmail.com>
2017-12-13 16:36:15 +0100

    add Calendar.js and routing

---
+++ src/Calendar.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react'
+
+
+class Calendar extends Component {
+
+  render () {
+    return (
+      <h2>Calendar</h2>
+
+    )
+  }
+}
+
+export default Calendar
--- src/index.js
+++ src/index.js
@@ -1,8 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from 'react-router-dom';
+
+import './index.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap-theme.css';
 import './index.css';
 import App from './App';
+import Calendar from './Calendar';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+ReactDOM.render(
+  <Router>
+    <div>
+      <Switch>
+        <Route exact path="/" component={App}/>
+        <Route path="/calendar" component={Calendar}/>
+      </Switch>
+      <p>&copy; 2017 Brodacze-Group</p>
+    </div>
+  </Router>,
+  document.getElementById('root')
+);
 registerServiceWorker();
