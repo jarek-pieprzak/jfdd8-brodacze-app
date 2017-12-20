f60481f05a7bc6b83b489841c002f3582f4330c6
Jaroslaw Pieprzak <jarekpiep@gmail.com>
2017-12-20 15:54:30 +0100

    repair order of items

--- src/index.js
+++ src/index.js
@@ -23,13 +23,14 @@ import Popover from './Popover';
 ReactDOM.render(
   <Router>
     <div>
-      {/*<Popover />*/}
-      <Charts/>
       <Switch>
         <Route exact path="/" component={App}/>
-        {/*<Route path="/donut" component={DonutChart}/>*/}
-        {/*<Route path="/bar" component={Bar}/>*/}
+        <Route path="/donut" component={DonutChart}/>
+        <Route path="/bar" component={Bar}/>
       </Switch>
+
+      <Popover />
+      <Charts/>
 
       <p>&copy; 2017 BrodaczeGroup</p>
     </div>
