085a8a2d7bd61865e5dd0b1ae6b27c1e7d475eb0
Maciej Szymański <szymanskimaciej@interia.eu>
2017-12-21 15:58:17 +0100

    add logpage directory, sign up & in forms, install firebase

---
+++ src/LogPage/LogPage.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+import firebase from 'firebase'
+import SignUp from './SignUp'
+import SignIn from './SignIn'
+
+
+class LogPage extends Component {
+
+  state = {
+    user: null
+  };
+
+  componentDidMount() {
+    firebase.auth().onAuthStateChanged(
+      user => this.setState({ user: user })
+    )
+  }
+
+  handleSignOut = () => {
+    firebase.auth().signOut()
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>ToDo Firebase</h1>
+        {
+          this.state.user === null ?
+            <div>
+              <SignUp/>
+              <SignIn/>
+            </div> :
+            <div>
+              Welcome to our app ! You will save money with Pocketbook !
+              <button onClick={this.handleSignOut}>Sign out</button>
+            </div>
+        }
+
+      </div>
+    )
+  }
+}
+export default LogPage;
+
+
+
--- src/index.js
+++ src/index.js
@@ -11,21 +11,30 @@ import 'bootstrap/dist/css/bootstrap-theme.css';
 
 import './index.css';
 
-import registerServiceWorker from './registerServiceWorker';
-
-import Logpage from './Logpage';
+import LogPage from './LogPage/LogPage';
 import Charts from './Charts';
 import DonutChart from './DonutChart';
 import Bar from './Bar';
 import Popover from './Popover';
+import registerServiceWorker from './registerServiceWorker';
 
+import firebase from 'firebase'
 
+const config = {
+  apiKey: "AIzaSyAkVekVE80S1geyyLg2FzZe_vjkC25Nk9w",
+  authDomain: "budget-app-brodacze-group.firebaseapp.com",
+  databaseURL: "https://budget-app-brodacze-group.firebaseio.com",
+  projectId: "budget-app-brodacze-group",
+  storageBucket: "budget-app-brodacze-group.appspot.com",
+  messagingSenderId: "284040962744"
+};
+firebase.initializeApp(config);
 
 ReactDOM.render(
   <Router>
     <div>
       <Switch>
-        <Route exact path="/logpage" component={Logpage}/>
+        <Route exact path="/logpage" component={LogPage}/>
         <Route path="/donut" component={DonutChart}/>
         <Route path="/bar" component={Bar}/>
       </Switch>
