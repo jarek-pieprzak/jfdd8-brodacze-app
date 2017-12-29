f0a6e93d2364d32675ab89a871359262369f7df6
Jarek <jarekpiep@gmail.com>
2017-12-29 14:27:19 +0100

    segregation of elements of charts

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ import './index.css';
 import setupFirebase from './setupFirebase'
 
 import Auth from './Auth'
-import Charts from './Charts';
+import Charts from './Charts/Charts';
 import Popover from './Popover';
 import List from './List';
 import registerServiceWorker from './registerServiceWorker';
