36e83e8cbabe51963cc3d62b2b32593ad6cd3f89
Jaroslaw Pieprzak <jarekpiep@gmail.com>
2018-01-09 16:47:53 +0100

    cleaning first data for render donut chart

--- src/DonutChart.js
+++ src/DonutChart.js
@@ -2,27 +2,6 @@ import React, {Component} from 'react';
 import {Doughnut} from 'react-chartjs-2';
 import firebase from 'firebase'
 
-// const data = {
-//   labels: [
-//     'Red',
-//     'Green',
-//     'Yellow'
-//   ],
-//   datasets: [{
-//     data: [300, 50, 100],
-//     backgroundColor: [
-//       '#FF6384',
-//       '#36A2EB',
-//       '#FFCE56'
-//     ],
-//     hoverBackgroundColor: [
-//       '#FF6384',
-//       '#36A2EB',
-//       '#FFCE56'
-//     ]
-//   }]
-// };
-
 class DonutChart extends Component {
   state = {
     entries: []
