aa148d406f530e362a6f9e6d810e04ee190ca1cf
MaciejSzymanski1 <szymanskimaciej@interia.eu>
2018-01-21 19:50:26 +0100

    fixes

--- src/Auth/SignUp.js
+++ src/Auth/SignUp.js
@@ -37,7 +37,7 @@ class SignUp extends Component {
   render() {
     return (
       <div>
-        <h1>Sign Up</h1>
+        <h2>Sign Up</h2>
         {this.state.error && <p style={{ color: 'red' }}>{this.state.error.message}</p>}
         <form
           onSubmit={this.handleSubmit}
